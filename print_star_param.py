602937e43b7d64009bedb8d2cffa21ca5dcec505
jongwhan kim <jjjong50@naver.com>
2020-07-14 17:41:24 +0900

    First Commit

---
+++ print_star_param.py
@@ -0,0 +1,7 @@
+def print_star(n):
+    for _ in range(n):
+        print("**********************")
+    # print('*******************************')
+
+
+print_star(4)
---
+++ sum_func.py
@@ -0,0 +1,9 @@
+def print_sum(a,b):
+    result = a + b
+    print(a, '과', b, '의 합은', result, '입니다.')
+
+print_sum(10, 20)
+print_sum(100,200)
+
+# print_sum(10)
+# print_sum(10, 20, 30)
